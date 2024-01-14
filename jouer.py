12ba3aba5a7dbdae388a27e547d4f3606a5c02b7
DUFF13 <mathis.dufrenois@hotmail.fr>
2024-01-14 22:26:11 +0100

    small commit

--- jouer.py
+++ jouer.py
@@ -213,11 +213,11 @@ def jouer_partie_IA(jeu : ttt.TTT, heuristic):
             start = time.time()
             print("\nTour de l'IA.")
             if heuristic == 1:
-                    _, meilleur_coup = jeu.min_max_align(3, float('-inf'), float('inf'), joueur)
+                    _, meilleur_coup = jeu.min_max_align(4, float('-inf'), float('inf'), joueur)
             elif heuristic == 2:
-                _, meilleur_coup = jeu.min_max_IterativDeepening(3, float('-inf'), float('inf'), joueur)
+                _, meilleur_coup = jeu.min_max_IterativDeepening(4, float('-inf'), float('inf'), joueur)
             elif heuristic == 4:
-                _, meilleur_coup = jeu.min_max_vide(3, float('-inf'), float('inf'), joueur) 
+                _, meilleur_coup = jeu.min_max_vide(4, float('-inf'), float('inf'), joueur) 
       
             jeu.play_move(meilleur_coup[0], meilleur_coup[1])
             print("durée du coup : " + str(time.time() - start))
@@ -258,11 +258,11 @@ def jouer_IA_vs_IA_test(jeu : ttt.TTT, heuristic1, heuristic2):
             meilleur_coup = None
             print("\nTour du joueur 1.")
             if heuristic1 == 1:
-                    _, meilleur_coup = jeu.min_max_align(3, float('-inf'), float('inf'), joueur)
+                    _, meilleur_coup = jeu.min_max_align(4, float('-inf'), float('inf'), joueur)
             elif heuristic1 == 2:
-                _, meilleur_coup = jeu.min_max_IterativDeepening(3, float('-inf'), float('inf'), joueur)
+                _, meilleur_coup = jeu.min_max_IterativDeepening(4, float('-inf'), float('inf'), joueur)
             elif heuristic1 == 4:
-                _, meilleur_coup = jeu.min_max_vide(3, float('-inf'), float('inf'), joueur) 
+                _, meilleur_coup = jeu.min_max_vide(4, float('-inf'), float('inf'), joueur) 
       
             if meilleur_coup is None:
                 print("none")
